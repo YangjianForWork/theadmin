476f799ada3baaa77042c42648aea5a948714343
YangjianForWork <yangjianfw@163.com>
2016-03-09 11:15:09 +0800

    test

---
+++ src/main/java/com/yang/thelab/common/dal/ibatis/IbatisCustomerDAO.java
@@ -0,0 +1,11 @@
+package com.yang.thelab.common.dal.ibatis;
+
+import com.yang.thelab.common.dal.CustomerDAO;
+/**
+ * 
+ * @author YJ.yang
+ * @version $Id: IbatisCustomerDAO.java, v 0.1 2016年3月9日 上午9:21:08 dev Exp $
+ */
+public class IbatisCustomerDAO implements CustomerDAO {
+    
+}
