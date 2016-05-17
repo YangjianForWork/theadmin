afa74b82072a7863f5446ed13408fc5b208381c1
YangjianForWork <yangjianfw@163.com>
2016-05-17 13:53:25 +0800

    代码更新

--- src/main/java/com/yang/thelab/common/enums/LabSiteStatus.java
+++ src/main/java/com/yang/thelab/common/enums/LabSiteStatus.java
@@ -8,7 +8,14 @@ import com.yang.thelab.common.EnumInterface;
  * @version $Id: LabSiteStatus.java, v 0.1 2016年5月17日 上午11:49:09 dev Exp $
  */
 public enum LabSiteStatus implements EnumInterface {
+    
+    INIT("INIT","初始化"),
     ;
+    
+    private LabSiteStatus(String code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
 
     private String code;
     private String desc;
